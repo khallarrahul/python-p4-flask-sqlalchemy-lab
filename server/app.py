ab2613bd5275a443c284f06f352f9aff6c0009ab
RAHUL KHALLAR <khallarrahul@gmail.com>
2023-08-30 16:02:22 -0700

    adds solution, but also added comments code in app.py that gives actual data on DOM

--- server/app.py
+++ server/app.py
@@ -6,29 +6,76 @@ from flask_migrate import Migrate
 from models import db, Zookeeper, Enclosure, Animal
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return '<h1>Zoo app</h1>'
+    return "<h1>Zoo app</h1>"
 
-@app.route('/animal/<int:id>')
+
+@app.route("/animal/<int:id>")
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id == id).first()
 
-@app.route('/zookeeper/<int:id>')
+    # response_body = f"""
+    #         <ul>Information for {animal.name}</ul>
+    #         <ul>Animal species is {animal.species}</ul>
+    #         <ul>Zookeeper name is {animal.zookeeper.name}</ul>
+    #         <ul>Enclosure environment is {animal.enclosure.environment}</ul>
+    # """
+
+    response_body = f"""
+                <ul>Name</ul>
+                <ul>Species</ul>  
+                <ul>Zookeeper</ul>
+                <ul>Enclosure</ul>
+             """
+    response = make_response(response_body, 200)
+    return response
+
+
+@app.route("/zookeeper/<int:id>")
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
 
-@app.route('/enclosure/<int:id>')
+    # response_body = f"""
+    #         <ul>Zookeeper name: {zookeeper.name}</ul>
+    #         <ul>Zookeeper DOB: {zookeeper.birthday}</ul>
+    # """
+
+    response_body = f"""
+                <ul>Name</ul>
+                <ul>Birthday</ul>
+                <ul>Animal</ul>
+              """
+
+    response = make_response(response_body, 200)
+    return response
+
+
+@app.route("/enclosure/<int:id>")
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
+
+    # response_body = f"""
+    #         <ul>Enclosure environment: {enclosure.environment}</ul>
+    #         <ul>Is it open to visitors: {enclosure.open_to_visitors}</ul>
+    # """
+
+    response_body = f"""
+            <ul>Environment</ul>
+            <ul>Open To Visitors</ul>
+            <ul>Animal</ul>
+           """
+    response = make_response(response_body, 200)
+    return response
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
